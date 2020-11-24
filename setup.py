727e2e2790b0b72c00786ed3466b574229992bdb
Niels Cautaerts <nielscautaerts@hotmail.com>
2020-11-24 10:22:12 +0100

    bumped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="tvipsconverter",
-    version="0.1.2",
+    version="0.1.3",
     description=(
         "GUI converter for movie data from TVIPS cameras"
         "into other formats like .blo, .tiff and .hspy."
